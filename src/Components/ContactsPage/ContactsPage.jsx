f9b5665d7e77711e6284ddfaec8bd603d5c47373
Alexander Sokolovskiy <sokolovskiya12@gmail.com>
2022-12-26 17:18:42 -0800

    minor changes to email from. preping for life actiom

--- src/Components/ContactsPage/ContactsPage.jsx
+++ src/Components/ContactsPage/ContactsPage.jsx
@@ -5,15 +5,15 @@ function ContactsPage() {
         <div className="contact">
             <h1 className='contact__heading'>contact me</h1>
             <form className='contact__form'>
-                <label className="contact__label">tell me who you are</label>
-                <input className='contact__input' type="text"/>
+                <label className="contact__label" >tell me who you are</label>
+                <input className='contact__input' type="text" placeholder='ypur name'/>
                 <label className="contact__label">how can i get back to you</label>
-                <input className='contact__input' type="email"/>
+                <input className='contact__input' type="email" placeholder='your email'/>
                 <input className='contact__input' type="tel" name="phone"
                         pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
-                        required/>
-                <label className="contact__label">tell me what`s up</label>
-                <input className="contact__input" type="textarea" rows="4" cols="50"/>
+                        required placeholder='your phone number'/>
+                <label className="contact__label">What problem I can help you with?</label>
+                <textarea className='contact__input' rows="5" cols="50" placeholder='your message'/>
                 <button type="submit" className="contact__button">DO IT!</button>
             </form>
         </div>
--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -13,9 +13,8 @@ function HomePage() {
                 <img className='hero__image' src={hero} alt='hero'/>
                 <div className='hero__text'>  
                     <div className='hero__name'>
-                        <h1>Sokol</h1>
-                        <h1>ov</h1>
-                        <h1>skiy</h1>
+                        <h1>Alex</h1>
+                        <h1>Sokolovskiy</h1>
                     </div>
                     <p className='hero__moto'>...help people surf the web</p>
                     <h1 className='hero__tag'>web developer</h1>
@@ -32,6 +31,7 @@ function HomePage() {
                     <div className='about'>
                         <div className='about__left'>
                             <img className="about__portrait" src={portrait} alt="portrait"/>
+                            <p>Hello. My name is Alexander Sokolovskiy. Im a junior web developer</p>
                         </div>
                         <div className='about__right'>
                             <h1 className="about__heading">About Me</h1>
