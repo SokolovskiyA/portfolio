add8455a9328bee1a7871e8131add0ad23282e17
Alexander Sokolovskiy <sokolovskiya12@gmail.com>
2022-12-30 20:51:28 -0800

    getting slowly clowser to a good look on about page

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -33,13 +33,27 @@ function HomePage() {
                     <div className='about'>
                         <div className='about__left'>
                             <img className="about__portrait" src={portrait} alt="portrait"/>
-                            <p className='about__left-text'>Hello and welcome to my story of great transformation and finding a new and amazing hobbie that changed my life.</p>
-                            <p className='about__left-text'>I started my professional life in service industry. For more then 10 years restaurants has been my work and passion. However, I`ve always </p>
+                            <p className='about__text'>This career path also helped me to develop proficiency and understanding of time management, sense of urgency,  task organization, process organization, communication, and most importantly ability to solve complex problems in a fast pace stressful environment. I could never guess that these skills will be so useful in my new path of Web Development</p>
                         </div>
                         <div className='about__right'>
                             <h1 className="about__heading">About Me</h1>
-                            <p className="about__title">"I try to influence life of many people by creating meaningfull and usefull technologies"</p>
-                            <img src={restaurant} alt="restaurant"/>
+                            <p className='about__text'>
+                            Hello and welcome to my story of career transformation. My name is Alex Sokolovskiy, Ive started my professional journey in restaurant industry and in 14 years developed my skills to become effective and knowledgeable manager. 
+                            </p>
+                            <img className='about__restaurant' src={restaurant} alt="restaurant"/>
+                        </div>
+                        <div>  
+                            <p className='about__text about__text-bottom'>And finally in September 2022 made a strong decision to elevate my skills to the next level by joining BrainStation web development diploma program. 
+                            The results were amazing. I gain knowledge and understanding of full cycle of web application development, brought my knowledge of javascript to a new level, learned ReactJS, NodeJS and build multiple complex full-stack projects that are ready to deploy. 
+                            Im confident in my skills and looking for opportunities to apply them in professional environment as soon as I can. 
+                            Im constantly looking for a new projects and would like to hear from you if you think i might be a good fit for your company. 
+                            </p>
+                        </div>
+                        <div className='education'>
+                            <img/>
+                            <img/>
+                            <img/>
+                            <img/>
                         </div>
                     </div>
                 </TabPanel>
