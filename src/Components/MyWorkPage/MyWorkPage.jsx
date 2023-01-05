bd28dd6b54dd349ccb61b70efee71ab6ed306f36
Alexander Sokolovskiy <sokolovskiya12@gmail.com>
2023-01-05 08:43:25 -0800

    mada a gif for a videos. testing on the phone screen

--- src/Components/MyWorkPage/MyWorkPage.jsx
+++ src/Components/MyWorkPage/MyWorkPage.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import pos from '../../assets/projects/PocketPos.mp4'
+import pos from '../../assets/projects/PocketPos.gif'
 import './MyWorkPage.scss'
 function MyWorkPage() {
     return (
         <div className='projects'>
             <h1 className='projects__heading'>my projects</h1>
             <div className='project'>
-                <video className='project__video' controls={false} src={pos} autoPlay muted/>
+                <img className='project__video' src={pos}/>
                 <div className='project__description'>
                     <h3 className='project__name'>Pocket - POS</h3>
                     <p className='project__text'>Full-Stack</p>
